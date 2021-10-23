8a9d1ac7cf80058430ad71e168ac448fd33cfd85
blenb <blenbb54@gmail.com>
2021-10-23 16:55:27 -0400

    merging issues

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -3,45 +3,39 @@ let slidePosition = 0;
 const slides = document.querySelectorAll('.carousel_item');
 const totalSlides = slides.length;
 
-document.
-   querySelector('#carousel_button--next')
-   .addEventListener("click", function() { console.log('test')
+document
+  .querySelector('#carousel_button--next')
+  .addEventListener('click', function() {
     moveToNextSlide();
-});
-document.
-   querySelector('#carousel_button--prev')
-   .addEventListener("click", function() {
+  });
+document
+  .querySelector('#carousel_button--prev')
+  // eslint-disable-next-line func-names
+  .addEventListener('click', function() {
     moveToPrevSlide();
-});
-function updateSlidePosition() { 
-    for(let slide of slides){
-        slide.classList.remove('carousel_item--visible');
-        slide.classList.add('carousel_item--hidden');
-
-    }  
-    slides[slidePosition].classList.add('carousel_item--visible')
+  });
+function updateSlidePosition() {
+  for (let slide of slides) {
+    slide.classList.remove('carousel_item--visible');
+    slide.classList.add('carousel_item--hidden');
+  }
+  slides[slidePosition].classList.add('carousel_item--visible');
 }
 
 function moveToNextSlide() {
-    if(slidePosition === totalSlides - 1) {
-        slidePosition = 0;
-    }else {
-        slidePosition++;
-    }
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition++;
+  }
 
-    updateSlidePosition();
-
-
+  updateSlidePosition();
 }
 function moveToPrevSlide() {
-
-    if(slidePosition === 0) {
-        slidePosition = totalSlides - 1;
-    }else {
-        slidePosition--;
-    }
-    updateSlidePosition();
-
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition--;
+  } 
+  updateSlidePosition(); 
 }
-
-
